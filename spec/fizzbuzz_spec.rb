c3fe9367078cae2c6a503a3e284713ca762e98c4
Rizwan Ali <rizcube@gmail.com>
2015-01-21 18:09:31 +0000

    refactored fizzbuzz_spec file

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,19 +1,25 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-	it "checks if the number is divisible by 3" do
-		expect(is_divisible_by_3?(3)).to be true
-	end
-end
 
-describe 'fizzbuzz' do
-	it "checks if the number is divisible by 5" do
-		expect(is_divisible_by_5?(5)).to be true
-	end
-end
+	context 'fizzbuzz can' do
 
-describe 'fizzbuzz' do
-	it 'checks if the number is divisible by 15' do
-		expect(is_divisible_by_3?(30)).to be true
+		it "checks if the number is divisible by 3" do
+			expect(is_divisible_by_3?(3)).to be true
+		end
+
+
+
+		it "checks if the number is divisible by 5" do
+			expect(is_divisible_by_5?(5)).to be true
+		end
+
+
+
+		it 'checks if the number is divisible by 15' do
+			expect(is_divisible_by_3?(30)).to be true
+		end
+
 	end
+
 end
